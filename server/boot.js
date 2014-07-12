2c8fd1e7f0a3fb141fff2b5ded851425964d4853
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-07-13 01:27:39 +0200

    Tests can now run more than once without restarting the Meteor app.

--- server/boot.js
+++ server/boot.js
@@ -1,6 +1,13 @@
+/* global
+ runServerTests: true,
+ VelocityTestReporter: false
+ */
+
 var fs = Npm.require('fs');
+var readFile = Meteor._wrapAsync(fs.readFile);
 var util = Npm.require('util');
 var path = Npm.require('path');
+var vm = Npm.require('vm');
 
 // boot code for jasmine
 var jasmineRequire = Npm.require('jasmine-core/lib/jasmine-core/jasmine.js');
@@ -10,11 +17,62 @@ var consoleFns = Npm.require('jasmine-core/lib/console/console.js');
 extend(jasmineRequire, consoleFns);
 jasmineRequire.console(jasmineRequire, jasmine);
 
-var isRunning = false
+var isRunning = false;
+
+var jasmineInterface = {
+  describe: function(description, specDefinitions) {
+    return jasmine.getEnv().describe(description, specDefinitions);
+  },
+
+  xdescribe: function(description, specDefinitions) {
+    return jasmine.getEnv().xdescribe(description, specDefinitions);
+  },
+
+  it: function(desc, func) {
+    return jasmine.getEnv().it(desc, func);
+  },
+
+  xit: function(desc, func) {
+    return jasmine.getEnv().xit(desc, func);
+  },
+
+  beforeEach: function(beforeEachFunction) {
+    return jasmine.getEnv().beforeEach(beforeEachFunction);
+  },
+
+  afterEach: function(afterEachFunction) {
+    return jasmine.getEnv().afterEach(afterEachFunction);
+  },
+
+  expect: function(actual) {
+    return jasmine.getEnv().expect(actual);
+  },
+
+  spyOn: function(obj, methodName) {
+    return jasmine.getEnv().spyOn(obj, methodName);
+  },
+
+  jsApiReporter: new jasmine.JsApiReporter({
+    timer: new jasmine.Timer()
+  })
+};
+
+extend(global, jasmineInterface);
+
+jasmine.addCustomEqualityTester = function(tester) {
+  jasmine.getEnv().addCustomEqualityTester(tester);
+};
+
+jasmine.addMatchers = function(matchers) {
+  return jasmine.getEnv().addMatchers(matchers);
+};
+
+jasmine.clock = function() {
+  return jasmine.getEnv().clock;
+};
 
 runServerTests = function () {
   if (isRunning) {
-    console.log('Already running');
     return;
   }
 
@@ -23,67 +81,11 @@ runServerTests = function () {
   // Force to create a new Env
   jasmine.currentEnv_ = null;
 
-  var env = jasmine.getEnv();
-
-  var jasmineInterface = {
-    describe: function(description, specDefinitions) {
-      return env.describe(description, specDefinitions);
-    },
-
-    xdescribe: function(description, specDefinitions) {
-      return env.xdescribe(description, specDefinitions);
-    },
-
-    it: function(desc, func) {
-      return env.it(desc, func);
-    },
-
-    xit: function(desc, func) {
-      return env.xit(desc, func);
-    },
-
-    beforeEach: function(beforeEachFunction) {
-      return env.beforeEach(beforeEachFunction);
-    },
-
-    afterEach: function(afterEachFunction) {
-      return env.afterEach(afterEachFunction);
-    },
-
-    expect: function(actual) {
-      return env.expect(actual);
-    },
-
-    spyOn: function(obj, methodName) {
-      return env.spyOn(obj, methodName);
-    },
-
-    jsApiReporter: new jasmine.JsApiReporter({
-      timer: new jasmine.Timer()
-    })
-  };
-
-  extend(global, jasmineInterface);
-
-  jasmine.addCustomEqualityTester = function(tester) {
-    env.addCustomEqualityTester(tester);
-  };
-
-  jasmine.addMatchers = function(matchers) {
-    return env.addMatchers(matchers);
-  };
-
-  jasmine.clock = function() {
-    return env.clock;
-  };
-
   // options from command line
   var isVerbose = true;
   var showColors = true;
 
   var specs = getSpecFiles(path.join(Velocity.getTestsPath(), 'jasmine', 'server'));
-
-  console.log('specs', specs);
 
   executeSpecs(specs, function () {
     isRunning = false;
@@ -91,7 +93,9 @@ runServerTests = function () {
 };
 
 function extend(destination, source) {
-  for (var property in source) destination[property] = source[property];
+  for (var property in source) {
+    destination[property] = source[property];
+  }
   return destination;
 }
 
@@ -99,10 +103,12 @@ function extend(destination, source) {
 function executeSpecs(specs, done, isVerbose, showColors) {
   global.jasmine = jasmine;
 
-  // TODO: Remove old specs. Maybe clone context.
   for (var i = 0; i < specs.length; i++) {
     var filename = specs[i];
-    Npm.require(filename.replace(/\.\w+$/, ""));
+    // Using vm.runInThisContext instead of require
+    // will always freshly load the tests from the file
+    var code = readFile(filename, {encoding: 'utf8'});
+    vm.runInThisContext(code, filename);
   }
 
   var env = jasmine.getEnv();
@@ -146,5 +152,5 @@ function getFiles(dir, matcher) {
 }
 
 function getSpecFiles(dir) {
-  return getFiles(dir, new RegExp("\\.js$"));
+  return getFiles(dir, new RegExp('\\.js$'));
 }
