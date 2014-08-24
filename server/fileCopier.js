8a2d6243af6526eedabe517c9c68e17f855782c9
alanning <alanning@gmail.com>
2014-08-24 12:39:36 -0400

    only copy client-side files via Velocity.FileCopier

--- server/fileCopier.js
+++ server/fileCopier.js
@@ -1,13 +1,25 @@
 /* global
  TEST_FRAMEWORK_NAME: false,
- Velocity: false
+ Velocity: false,
+ runServerTests: false
  */
+var _ = Npm.require('lodash')
+
+var rerunTests = _.throttle(runServerTests, 100)
 
 if (process.env.NODE_ENV === 'development') {
   Meteor.startup(function () {
     var fileCopier = new Velocity.FileCopier({
-      targetFramework: TEST_FRAMEWORK_NAME
-    });
-    fileCopier.start();
-  });
+      targetFramework: TEST_FRAMEWORK_NAME,
+      onFileAdded: rerunTests,
+      onFileChanged: rerunTests,
+      onFileRemoved: rerunTests,
+      shouldCopy: function (filepath) {
+        var isClient = filepath.absolutePath.indexOf('server') === -1
+
+        return isClient
+      }
+    })
+    fileCopier.start()
+  })
 }
