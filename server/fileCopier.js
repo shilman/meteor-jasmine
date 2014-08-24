eaae64bccfd237ab695e92df1bde32db52b3b493
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-08-24 16:14:20 +0200

    Use _.throttle for test run throttling

--- server/fileCopier.js
+++ server/fileCopier.js
@@ -3,29 +3,18 @@
  Velocity: false,
  runServerTests: false
  */
+var _ = Npm.require('lodash')
 
-var TEST_RUN_WAIT_TIME = 100;
-var rerunTimer = null;
-
-function rerunTests() {
-  runServerTests();
-}
-
-function startTestsRerunTimeout() {
-  if (rerunTimer) {
-    Meteor.clearTimeout(rerunTimer);
-  }
-  rerunTimer = Meteor.setTimeout(rerunTests, TEST_RUN_WAIT_TIME);
-}
+var rerunTests = _.throttle(runServerTests, 100)
 
 if (process.env.NODE_ENV === 'development') {
   Meteor.startup(function () {
     var fileCopier = new Velocity.FileCopier({
       targetFramework: TEST_FRAMEWORK_NAME,
-      onFileAdded: startTestsRerunTimeout,
-      onFileChanged: startTestsRerunTimeout,
-      onFileRemoved: startTestsRerunTimeout
-    });
-    fileCopier.start();
-  });
+      onFileAdded: rerunTests,
+      onFileChanged: rerunTests,
+      onFileRemoved: rerunTests
+    })
+    fileCopier.start()
+  })
 }
