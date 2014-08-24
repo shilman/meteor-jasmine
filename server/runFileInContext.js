c58c7bfe464b03460344e67bc0d03430f85aa2c4
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-08-24 16:17:49 +0200

    Handle syntax error when executing test code with vm.runInContext

--- server/runFileInContext.js
+++ server/runFileInContext.js
@@ -1,13 +1,21 @@
-var fs = Npm.require('fs');
-var readFile = Meteor._wrapAsync(fs.readFile);
-var vm = Npm.require('vm');
+/* global
+   logError: falses
+ */
+
+var fs = Npm.require('fs')
+var readFile = Meteor._wrapAsync(fs.readFile)
+var vm = Npm.require('vm')
 
 runFileInContext = function (filename, context) {
-  var code = readFile(filename, {encoding: 'utf8'});
-  return vm.runInContext(code, context, filename);
-};
+  var code = readFile(filename, {encoding: 'utf8'})
+  try {
+    vm.runInContext(code, context, filename)
+  } catch(error) {
+    logError('The file "' + filename + '" has syntax errors.')
+  }
+}
 
 runFileInThisContext = function (filename) {
-  var code = readFile(filename, {encoding: 'utf8'});
-  return vm.runInThisContext(code, filename);
-};
+  var code = readFile(filename, {encoding: 'utf8'})
+  return vm.runInThisContext(code, filename)
+}
