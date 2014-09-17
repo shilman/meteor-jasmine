a325cc5903935b0068b9031f8b1e070f824aa9b0
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-09-18 01:34:34 +0200

    Fixes #15 and #16

--- server/fileCopier.js
+++ server/fileCopier.js
@@ -1,19 +1,27 @@
 /* global
  TEST_FRAMEWORK_NAME: false,
  Velocity: false,
- runServerTests: false
+ runServerTests: false,
+ VelocityTestFiles: false
  */
-var _ = Npm.require('lodash')
-
-var rerunTests = _.throttle(Meteor.bindEnvironment(runServerTests, 'runServerTests'), 100)
 
 if (process.env.NODE_ENV === 'development') {
+  var _ = Npm.require('lodash')
+  var rerunServerTests = _.throttle(Meteor.bindEnvironment(runServerTests, 'runServerTests'), 100)
+
   Meteor.startup(function () {
+    var testFilesCursor = VelocityTestFiles.find({
+      targetFramework: TEST_FRAMEWORK_NAME
+    })
+
+    var testFilesObserver = testFilesCursor.observe({
+      added: rerunServerTests,
+      changed: rerunServerTests,
+      removed: rerunServerTests
+    })
+
     var fileCopier = new Velocity.FileCopier({
       targetFramework: TEST_FRAMEWORK_NAME,
-      onFileAdded: rerunTests,
-      onFileChanged: rerunTests,
-      onFileRemoved: rerunTests,
       shouldCopy: function (filepath) {
         var isClient = filepath.absolutePath.indexOf('server') === -1
 
