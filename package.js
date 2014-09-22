7a7ea7cb22add7a74580ff4f597fd733d2245ad5
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-09-22 05:29:54 +0200

    Bum version to 0.3.5

--- package.js
+++ package.js
@@ -6,7 +6,7 @@
 Package.describe({
   name: 'sanjo:jasmine',
   summary: 'Easily use Jasmine in Meteor',
-  version: '0.3.4',
+  version: '0.3.5',
   git: 'https://github.com/Sanjo/meteor-jasmine.git'
 })
 
