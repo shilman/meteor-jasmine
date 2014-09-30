0389b8bbb058aa2347feda276c9ae3d585d530b7
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-09-30 18:09:24 +0200

    Bump version to 0.4.6
    Adds support for Coffeescript specs in server unit test mode.

--- package.js
+++ package.js
@@ -6,7 +6,7 @@
 Package.describe({
   name: 'sanjo:jasmine',
   summary: 'Easily use Jasmine in Meteor',
-  version: '0.4.5',
+  version: '0.4.6',
   git: 'https://github.com/Sanjo/meteor-jasmine.git'
 })
 
