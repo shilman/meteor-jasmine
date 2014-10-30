2d9a519849b201d423a85354516f12fad5df63c2
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-10-30 22:01:57 +0100

    Bumps version to 0.5.0-rc.2

--- package.js
+++ package.js
@@ -6,7 +6,7 @@
 Package.describe({
   name: 'sanjo:jasmine',
   summary: 'Easily use Jasmine in Meteor',
-  version: '0.5.0-rc.1',
+  version: '0.5.0-rc.2',
   git: 'https://github.com/Sanjo/meteor-jasmine.git',
   debugOnly: true
 })
